a6b036a76c57a8ec2afb16117e3c0ea8f77e5c90
jotirambandgar <bandgar09@gmail.com>
2019-12-16 15:14:35 +0530

    Added : initial board for starting game

--- ticTacToe.sh
+++ ticTacToe.sh
@@ -1,5 +1,25 @@
 #!/bin/bash -x
+gameBoard=( 1 2 3 4 5 6 7 8 9 );
+
+function getGameBoard(){
+	for block in ${gameBoard[@]}
+	do
+		echo -n "$block |"
+		
+		if [ $(( $block%3 )) -eq 0 ]
+		then
+			if [ $block -ne 9 ]
+			then
+				echo  -e "\n__________"
+			else
+				echo -e "\n"
+			fi
+		fi
+	done
+}
+
 function main(){
 	echo "Welcome to Tic tac toe"
+	getGameBoard
 }
 main
