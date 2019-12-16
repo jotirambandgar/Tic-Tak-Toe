4d9dbf06ce87a652b2850eecac47d0610ac5cb6b
jotirambandgar <bandgar09@gmail.com>
2019-12-16 18:43:38 +0530

    Added : functionality to select cell

--- ticTacToe.sh
+++ ticTacToe.sh
@@ -1,15 +1,16 @@
 #!/bin/bash -x
-gameBoard=( 1 2 3 4 5 6 7 8 9 );
+gameBoard=( "1" "2" "3" "4" "5" "6" "7" "8" "9" );
 
 function getGameBoard(){
-	for block in ${gameBoard[@]}
+	 echo "_ _ _ _ _ _"
+	for (( i=0; i<${#gameBoard[@]}; i++ ))
 	do
-		echo -n "$block |"
-		if [ $(( $block%3 )) -eq 0 ]
+		echo -n "${gameBoard[$i]}  |"
+		if [ $(( $(($i+1))%3 )) -eq 0 ]
 		then
-			if [ $block -ne 9 ]
+			if [ $i != "9" ]
 			then
-				echo  -e "\n__________"
+				echo  -e "\n_ _ _ _ _ _"
 			else
 				echo -e "\n"
 			fi
@@ -17,17 +18,26 @@ function getGameBoard(){
 	done
 }
 
+function selectCell(){
+	if [ $player == "X" ]
+	then
+		read -p "Enter your choice" choice
+		gameBoard[$(( $choice - 1 ))]="X"
+		getGameBoard
+	fi
+}
+
 function main(){
 	echo "Welcome to Tic tac toe"
 	getGameBoard
 	if [ $(( RANDOM%2 )) -eq 1 ]
 	then
-		player=X
+		player="X"
 		echo "X is Assign to user and player play first"
 	else
-		player=O
+		player="O"
       echo "O is Assign to user and computer play first"
 	fi
-
+	selectCell
 }
 main
